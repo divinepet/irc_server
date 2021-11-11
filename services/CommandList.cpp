6f2b3d91902bec31d8090d21d6ec82800db5e1da
Frankie Ariel <frariel@ev-e6.kzn.21-school.ru>
2021-11-11 14:08:56 +0300

    Admin args checking updated

--- services/CommandList.cpp
+++ services/CommandList.cpp
@@ -2,17 +2,13 @@
 #include "CommandList.hpp"
 
 void	CommandList::admin(std::vector<std::string> command, User& user) {
-	if (command.size() != 1) {
-		if (command[1] != serverInfo::serverName) {
-			Server::writing(user.getSocketFd(), Service::formatMsg(402, "No such server", user));
-			return;
-		}
-	} else {
+	if (command.size() == 1 || (command.size() != 1 && command[1] == serverInfo::serverName)) {
 		Server::writing(user.getSocketFd(), Service::formatMsg(256, "Administrative info", user, serverInfo::serverName));
 		Server::writing(user.getSocketFd(), Service::formatMsg(257,  "Nickname    TestNickname", user));
 		Server::writing(user.getSocketFd(), Service::formatMsg(258,  "Name        TestName", user));
 		Server::writing(user.getSocketFd(), Service::formatMsg(259,  "E-Mail      Test@test.com", user));
-	}
+	} else
+		Server::writing(user.getSocketFd(), Service::formatMsg(402, "No such server", user));
 }
 
 void CommandList::nick(std::vector<std::string> args, User& user) {
